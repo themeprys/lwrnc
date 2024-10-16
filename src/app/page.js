e6b414da0071f554e8f682e53ef37f23fc209cb2
themeprys <themeprys@gmail.com>
2024-10-17 07:59:20 +0800

    add hero

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import Image from 'next/image'
 import logoRecruitable from '/public/img/logo.webp'
 import headlineText from '/public/img/headline.webp'
 import heroImg from '/public/img/heroimg.webp'
+import iconOne from '/public/img/icon_1.webp'
+import iconTwo from '/public/img/icon_2.webp'
+
+
 import { Montserrat } from 'next/font/google'
 
 const montserrat = Montserrat({ 
@@ -9,6 +13,13 @@ const montserrat = Montserrat({
   display: 'swap',
  })
 
+ export const metadata = {
+  title: 'RecruitABLE',
+  openGraph: {
+    title: 'Recruitable',
+  },
+}
+ 
 
 export default function Home() {
   return (
@@ -18,27 +29,44 @@ export default function Home() {
           <div className="col-sm p-5">
             <Image
               src={logoRecruitable}
-              alt="Picture of the author"
-              width={300} 
+              alt="Recruitable"
+              width={300}
+              sizes="100vw"
+              style={{
+                width: '100%',
+                height: 'auto',
+              }}                             
             />
           </div>
         </div>
         <div className="row">
-          <div className="col p-5">
+          <div className="col-12 col-md-6 p-5">
             <div>
                 <Image
                 src={headlineText}
-                alt="Picture of the author"
+                alt="Recruitable"
                 width={500}
+                sizes="100vw"
+                style={{
+                  width: '100%',
+                  height: 'auto',
+                }}              
+  
               />
             </div>
             <p>Swipe, terhubung dan mulai karirmu di Industri Pariwisata!</p>
           </div>
-          <div className="col">               
+          <div className="col-12 col-md-6">               
             <Image
                 src={heroImg}
-                alt="Picture of the author"
+                alt="Recruitable"
                 width={600}
+                sizes="100vw"
+                style={{
+                  width: '100%',
+                  height: 'auto',
+                }}              
+  
             />
           </div>
         </div> 
@@ -46,23 +74,47 @@ export default function Home() {
           <div className="col-12">
             <h3>MENANGKAN PROGRAM<br/><span>PELATIHAN EKSKLUSIF</span><br/>BERSAMA LAWRENCE BENSON</h3>
           </div>
-          <div className="col-12">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/GqxhlYPwAIU?si=DpQll0ttZTLiMBm9" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-          </div>
-          {/* <div className="col-12">
-          Text Lawrence 2
-          </div> */}
-          {/* <div className="col-12">
-          <div className="row">
-            <div className="col">Text 1</div>
-            <div className="col">Text 2</div>            
-          </div>
-          <div className="col-12">
-          Text Lawrence 3
-          </div>
-          </div>           */}
           </div>       
       </div>
-    </div>
+      <div className='container-fluid p-0'>
+      < iframe width="100%" height="350" src="https://www.youtube.com/embed/GqxhlYPwAIU?si=DpQll0ttZTLiMBm9"  frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+      <div className='container'>
+        <div className="col-12 text-center lwrn_win2">
+        Download aplikasi Recruit<strong>ABLE</strong> dan dapatkan kesempatan menjadi salah satu dari <span>5 orang beruntung</span> yang terpilih mengikuti 
+        </div>
+        <div className="col-12 text-center lwrn_benefit">
+        <Image
+                src={iconOne}
+                alt="Recruitable"
+                width={200}
+                sizes="100vw"
+                style={{
+                  width: '30%',
+                  height: 'auto',
+                }}              
+  
+            />
+          <h4 className='pt-4'>Pelatihan eksklusif dengan Lawrence Benson</h4>      
+        </div>
+        <div className="col-12 text-center lwrn_benefit">
+        <Image
+                src={iconTwo}
+                alt="Recruitable"
+                width={200}
+                sizes="100vw"
+                style={{
+                  width: '30%',
+                  height: 'auto',
+                }}              
+  
+            />
+          <h4 className='pt-4'>Tiket pesawat gratis ke Bali</h4>      
+        </div>
+        <div className="col-12 text-center lwrn_win2 px-5 pt-0">
+                Ini adalah kesempatan emas yang akan membawamu ke tempat-tempat terbaik di industri pariwisata di Bali!
+        </div>
+      </div>
+    </div>       
   );
 }
